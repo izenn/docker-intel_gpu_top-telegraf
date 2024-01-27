4c5a6d2ba9c2e03971274eab1b0eadbd251962ab
freender <40869255+freender@users.noreply.github.com>
2024-01-27 01:36:45 -0500

    Update get_intel_gpu_status.sh

--- opt/intel-gpu-telegraf/get_intel_gpu_status.sh
+++ opt/intel-gpu-telegraf/get_intel_gpu_status.sh
@@ -4,7 +4,8 @@
 
 #Beat intel_gpu_top into submission
 JSON=$(/usr/bin/timeout -k 3 3 /usr/bin/intel_gpu_top -J)
-VIDEO_UTIL=$(echo "$JSON"|grep Video/0 -A 1|tail -1|grep busy|cut -d ":" -f2|cut -d "," -f1|cut -d " " -f2)
+VIDEO_UTIL=$(echo "$JSON"|grep busy|sort|tail -1|cut -d ":" -f2|cut -d "," -f1|cut -d " " -f2)
+
 
 #Spit out something telegraf can work with
 echo "[{\"time\": `date +%s`, \"intel_gpu_util\": "$VIDEO_UTIL"}]"
